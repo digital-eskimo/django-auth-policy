498c07eb263624e9fb8d5680b7fee11b489627b8
Simon Meers <simon@simonmeers.com>
2016-01-27 09:13:09 +1100

    make migrations Django-1.6-friendly

--- django_auth_policy/migrations/0001_initial.py
+++ django_auth_policy/migrations/0001_initial.py
@@ -1,69 +1,130 @@
-# encoding: utf-8
-from __future__ import unicode_literals
-
-from django.db import models, migrations
-import django.db.models.deletion
-from django.conf import settings
-import django.utils.timezone
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
 
 
-class Migration(migrations.Migration):
+class Migration(SchemaMigration):
 
-    dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-    ]
+    def forwards(self, orm):
+        # Adding model 'LoginAttempt'
+        db.create_table(u'django_auth_policy_loginattempt', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('username', self.gf('django.db.models.fields.CharField')(max_length=100, db_index=True)),
+            ('source_address', self.gf('django.db.models.fields.GenericIPAddressField')(max_length=39, db_index=True)),
+            ('hostname', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('successful', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('timestamp', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, db_index=True, blank=True)),
+            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'], null=True, on_delete=models.SET_NULL, blank=True)),
+            ('user_repr', self.gf('django.db.models.fields.CharField')(max_length=200, blank=True)),
+            ('lockout', self.gf('django.db.models.fields.BooleanField')(default=True)),
+        ))
+        db.send_create_signal(u'django_auth_policy', ['LoginAttempt'])
 
-    operations = [
-        migrations.CreateModel(
-            name='LoginAttempt',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('username', models.CharField(max_length=100, verbose_name='username', db_index=True)),
-                ('source_address', models.GenericIPAddressField(verbose_name='source address', db_index=True)),
-                ('hostname', models.CharField(max_length=100, verbose_name='hostname')),
-                ('successful', models.BooleanField(default=False, verbose_name='successful')),
-                ('user', models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, verbose_name='user', to_field='id', blank=True, to=settings.AUTH_USER_MODEL, null=True)),
-                ('timestamp', models.DateTimeField(default=django.utils.timezone.now, auto_now_add=True, verbose_name='timestamp', db_index=True)),
-                ('lockout', models.BooleanField(default=True, help_text='Counts towards lockout count', verbose_name='lockout')),
-            ],
-            options={
-                'ordering': (b'-id',),
-                'verbose_name': 'login attempt',
-                'verbose_name_plural': 'login attempts',
-                'permissions': ((b'unlock', 'Unlock by username or IP address'),),
-            },
-            bases=(models.Model,),
-        ),
-        migrations.CreateModel(
-            name='PasswordChange',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=django.db.models.deletion.PROTECT, to_field='id', verbose_name='user')),
-                ('timestamp', models.DateTimeField(default=django.utils.timezone.now, verbose_name='timestamp', auto_now_add=True)),
-                ('successful', models.BooleanField(default=False, verbose_name='successful')),
-                ('is_temporary', models.BooleanField(default=False, verbose_name='is temporary')),
-                ('password', models.CharField(default=b'', verbose_name='password', max_length=128, editable=False)),
-            ],
-            options={
-                'ordering': (b'-id',),
-                'verbose_name': 'password change',
-                'verbose_name_plural': 'password changes',
-            },
-            bases=(models.Model,),
-        ),
-        migrations.CreateModel(
-            name='UserChange',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=django.db.models.deletion.PROTECT, to_field='id', verbose_name='user')),
-                ('timestamp', models.DateTimeField(auto_now_add=True, verbose_name='timestamp')),
-                ('by_user', models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=django.db.models.deletion.PROTECT, to_field='id', verbose_name='by user')),
-            ],
-            options={
-                'ordering': (b'-id',),
-                'verbose_name': 'user change',
-                'verbose_name_plural': 'user changes',
-            },
-            bases=(models.Model,),
-        ),
-    ]
+        # Adding model 'PasswordChange'
+        db.create_table(u'django_auth_policy_passwordchange', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'], null=True, on_delete=models.SET_NULL, blank=True)),
+            ('user_repr', self.gf('django.db.models.fields.CharField')(max_length=200)),
+            ('timestamp', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('successful', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('is_temporary', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('password', self.gf('django.db.models.fields.CharField')(default='', max_length=128)),
+        ))
+        db.send_create_signal(u'django_auth_policy', ['PasswordChange'])
+
+        # Adding model 'UserChange'
+        db.create_table(u'django_auth_policy_userchange', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'], null=True, on_delete=models.SET_NULL, blank=True)),
+            ('user_repr', self.gf('django.db.models.fields.CharField')(max_length=200)),
+            ('timestamp', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('by_user', self.gf('django.db.models.fields.related.ForeignKey')(blank=True, related_name='changed_users', null=True, on_delete=models.SET_NULL, to=orm['auth.User'])),
+            ('by_user_repr', self.gf('django.db.models.fields.CharField')(max_length=200)),
+        ))
+        db.send_create_signal(u'django_auth_policy', ['UserChange'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'LoginAttempt'
+        db.delete_table(u'django_auth_policy_loginattempt')
+
+        # Deleting model 'PasswordChange'
+        db.delete_table(u'django_auth_policy_passwordchange')
+
+        # Deleting model 'UserChange'
+        db.delete_table(u'django_auth_policy_userchange')
+
+
+    models = {
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'django_auth_policy.loginattempt': {
+            'Meta': {'ordering': "('-id',)", 'object_name': 'LoginAttempt'},
+            'hostname': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'lockout': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'source_address': ('django.db.models.fields.GenericIPAddressField', [], {'max_length': '39', 'db_index': 'True'}),
+            'successful': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'timestamp': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'db_index': 'True', 'blank': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']", 'null': 'True', 'on_delete': 'models.SET_NULL', 'blank': 'True'}),
+            'user_repr': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'max_length': '100', 'db_index': 'True'})
+        },
+        u'django_auth_policy.passwordchange': {
+            'Meta': {'ordering': "('-id',)", 'object_name': 'PasswordChange'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_temporary': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'password': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '128'}),
+            'successful': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'timestamp': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']", 'null': 'True', 'on_delete': 'models.SET_NULL', 'blank': 'True'}),
+            'user_repr': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        },
+        u'django_auth_policy.userchange': {
+            'Meta': {'ordering': "('-id',)", 'object_name': 'UserChange'},
+            'by_user': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'changed_users'", 'null': 'True', 'on_delete': 'models.SET_NULL', 'to': u"orm['auth.User']"}),
+            'by_user_repr': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'timestamp': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']", 'null': 'True', 'on_delete': 'models.SET_NULL', 'blank': 'True'}),
+            'user_repr': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        }
+    }
+
+    complete_apps = ['django_auth_policy']
